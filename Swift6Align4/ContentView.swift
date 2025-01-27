51dee77e8466cca6b352317e2c8356fcd8b9ab60
rsazra <rajbir.singh.azra@gmail.com>
2025-01-27 00:06:25 -0500

    Initial Commit

---
+++ Swift6Align4/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Swift6Align4
+//
+//  Created by Rajbir Singh Azra on 2025-01-27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Swift6Align4/Swift6Align4App.swift
@@ -0,0 +1,17 @@
+//
+//  Swift6Align4App.swift
+//  Swift6Align4
+//
+//  Created by Rajbir Singh Azra on 2025-01-27.
+//
+
+import SwiftUI
+
+@main
+struct Swift6Align4App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
