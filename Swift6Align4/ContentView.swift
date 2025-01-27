36b3b622a44b26a84361726473d8af562d0ac2d2
rsazra <rajbir.singh.azra@gmail.com>
2025-01-27 17:29:51 -0500

    testing

--- Swift6Align4/ContentView.swift
+++ Swift6Align4/ContentView.swift
@@ -10,12 +10,11 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            RoundedRectangle(cornerRadius: /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Corner Radius@*/10.0/*@END_MENU_TOKEN@*/)
+                .frame(width: 100, height: 50)
+                .foregroundColor(.blue)
+            Circle().frame(width: 50)
         }
-        .padding()
     }
 }
 
