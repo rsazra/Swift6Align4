5326bdc981ccacc78481c4adf627fe546020009c
rsazra <rajbir.singh.azra@gmail.com>
2025-03-04 13:10:48 -0500

    glowing potential drop and winning chips

--- Swift6Align4/GameBoardView.swift
+++ Swift6Align4/GameBoardView.swift
@@ -21,11 +21,14 @@ struct GameView: View {
     @State var player: Player = .red
     @State private var wins: [Player: Int] = [.red: 0, .yellow: 0, .none: 0]
     @State private var winner: Player? = nil
+    @State private var winningChips: [(Int, Int)] = []
     @State private var playCount: Int = 0
 
     let startingChipOffset: CGSize = CGSize(width: 0, height: -235)
     @State var currentChipOffset: CGSize = CGSize(width: 0, height: -235)
     @State var isAnimating: Bool = false
+
+    @State var currentColumn: Int = -1
 
     var body: some View {
         VStack {
@@ -47,7 +50,7 @@ struct GameView: View {
     func dropChip() {
         if isAnimating { return }
 
-        let column = Int(currentChipOffset.width / 48 + 3)
+        let column = currentColumn
         let columnChips = board[column]
         let last = columnChips.lastIndex(of: .none)
 
@@ -89,33 +92,37 @@ struct GameView: View {
 
     func checkEnd(column: Int, row: Int) {
         print(playCount)
-        if playCount == 42 {print("draw")}
+        if playCount == 42 { print("draw") }
         // only check around where the latest piece was dropped
         // check 4 verticals
         for i in 0..<4 {
             if (row + i - 3) >= 0, (row + i) < 6 {
-                if
-                    board[column][row + i] == player,
+                if board[column][row + i] == player,
                     board[column][row + i - 1] == player,
                     board[column][row + i - 2] == player,
                     board[column][row + i - 3] == player
                 {
+                    for j in 0..<4 {
+                        winningChips.append((column, row + i - j))
+                    }
                     print("done")
                     winner = player
                     winner == .red ? print("red") : print("yellow")
                 }
             }
         }
-        
+
         // check 4 horizontals
         for i in 0..<4 {
             if (column + i - 3) >= 0, (column + i) < 7 {
-                if
-                    board[column + i][row] == player,
+                if board[column + i][row] == player,
                     board[column + i - 1][row] == player,
                     board[column + i - 2][row] == player,
                     board[column + i - 3][row] == player
                 {
+                    for j in 0..<4 {
+                        winningChips.append((column + i - j, row))
+                    }
                     print("row done")
                     winner = player
                     winner == .red ? print("red") : print("yellow")
@@ -125,13 +132,17 @@ struct GameView: View {
 
         // check 4 descending diagonal
         for i in 0..<4 {
-            if (column + i - 3) >= 0, (column + i) < 7, (row + i - 3) >= 0, (row + i) < 6 {
-                if
-                    board[column + i][row + i] == player,
+            if (column + i - 3) >= 0, (column + i) < 7, (row + i - 3) >= 0,
+                (row + i) < 6
+            {
+                if board[column + i][row + i] == player,
                     board[column + i - 1][row + i - 1] == player,
                     board[column + i - 2][row + i - 2] == player,
                     board[column + i - 3][row + i - 3] == player
                 {
+                    for j in 0..<4 {
+                        winningChips.append((column + i - j, row + i - j))
+                    }
                     print("diagonal down done")
                     winner = player
                     winner == .red ? print("red") : print("yellow")
@@ -141,13 +152,17 @@ struct GameView: View {
 
         // check 4 ascending diagonals
         for i in 0..<4 {
-            if (column - i) >= 0, (column - i + 3) < 7, (row + i - 3) >= 0, (row + i) < 6 {
-                if
-                    board[column - i][row + i] == player,
+            if (column - i) >= 0, (column - i + 3) < 7, (row + i - 3) >= 0,
+                (row + i) < 6
+            {
+                if board[column - i][row + i] == player,
                     board[column - i + 1][row + i - 1] == player,
                     board[column - i + 2][row + i - 2] == player,
                     board[column - i + 3][row + i - 3] == player
                 {
+                    for j in 0..<4 {
+                        winningChips.append((column - i + j, row + i - j))
+                    }
                     print("diagonal up done")
                     winner = player
                     winner == .red ? print("red") : print("yellow")
@@ -178,12 +193,14 @@ struct GameView: View {
 
                     currentChipOffset.width = snapChipToGrid(
                         currentOffset: newChipOffset)
+                    currentColumn = Int(currentChipOffset.width / 48 + 3)
                 })
                 .onEnded({
                     value in
                     if value.location.x > 0 && value.location.x < 360 {
                         dropChip()
                     }
+                    currentColumn = -1
                 }))
     }
 
@@ -199,14 +216,17 @@ struct GameView: View {
             ForEach(0..<rows, id: \.self) { row in
                 HStack {
                     ForEach(0..<columns, id: \.self) { column in
+                        let highlightColor = column == currentColumn ? Color.orange : Color.clear
                         let chip = board[column][row]
+                        let winningChip = winner != nil ? winningChips.contains {$0 == (column, row)} : false
                         let color =
                             chip == .none
-                            ? Color.clear
+                            ? highlightColor
                             : chip == .red ? Color.red : Color.yellow
                         Circle()
                             .frame(width: 40, height: 50)
                             .foregroundColor(color)
+                            .glow(chip == .none || winningChip)
                     }
                 }
             }
@@ -241,6 +261,27 @@ struct GameView: View {
     private var ScoreCardView: some View {
         Text("scoreCardText")
             .frame(alignment: .topLeading)
+    }
+}
+
+struct Glow: ViewModifier {
+    func body(content: Content) -> some View {
+        ZStack {
+            content.blur(radius: 10)
+            content
+        }
+    }
+}
+
+extension View {
+    @ViewBuilder
+    func glow(_ glowing: Bool) -> some View {
+        if glowing {
+            self.modifier(Glow())
+        }
+        else {
+            self
+        }
     }
 }
 
